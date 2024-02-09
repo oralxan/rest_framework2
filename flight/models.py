612028da4946d47cfe79fb38950dced7b60d0263
oralxan <oralxan@users.noreply.github.com>
2024-02-09 15:05:37 +0500

    newup

--- flight/models.py
+++ flight/models.py
@@ -1,35 +1,35 @@
-from django.db.models import *
+from django.db import models
 from plane.models import Plane
-# from airline.models import Airline
-class Flight(Model):
-    froms = CharField(
-        verbose_name= 'from',
-        max_length= 512
-    )
-    to = CharField(
-        verbose_name= 'characteristics',
+from airline.models import Airline
+
+class Flight(models.Model):
+    froms = models.CharField(
+        verbose_name='From',
         max_length=512
     )
-    plane = ForeignKey(
+    to = models.CharField(
+        verbose_name='To',
+        max_length=512
+    )
+    plane = models.ForeignKey(
         Plane,
         verbose_name='Plane',
-        on_delete=PROTECT
-        
+        on_delete=models.PROTECT,
+        related_name='flights'
     )
-    # airline = ForeignKey(
-    #     Airline,
-    #     verbose_name='Airline',
-    #     on_delete=PROTECT
-        
-    # )
-    price = PositiveBigIntegerField(
-        verbose_name= 'Price of the price',
+    airline = models.ForeignKey(
+        Airline,
+        verbose_name='Airline',
+        on_delete=models.PROTECT
+    )
+    price = models.PositiveBigIntegerField(
+        verbose_name='Price',
         default=0,
     )
 
-
     def __str__(self):
         return f'{self.froms}'
+
     class Meta:
         verbose_name = 'Flight'
         verbose_name_plural = 'Flights'
